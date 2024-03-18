c578f84507e6bfc1aca3dcd9bcdbd2f97ae23d2d
ilham majid zaiman <ilhammajidzaiman07@gmail.com>
2024-03-18 22:02:23 +0700

    feat: add seeder role permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,10 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            RoleHasPermissionSeeder::class,
+            ModelHasRoleSeeder::class,
             UserSeeder::class,
         ]);
     }
