211c8eeeaeba72e0031821f82549e78926d84587
ilhammajidzaiman <ilhammajidzaiman07@gmail.com>
2025-05-28 15:17:50 +0700

    FIX: route for developer

--- routes/developer.php
+++ routes/developer.php
@@ -5,9 +5,9 @@ use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(
+Route::middleware('auth')->prefix('/developer')->group(
     function () {
-        Route::get("/make-role", function () {
+        Route::name('generate.role')->get("/generate-role", function () {
             try {
                 Role::firstOrCreate(['name' => RoleEnum::SuperAdmin]);
                 Role::firstOrCreate(['name' => RoleEnum::Admin]);
@@ -16,6 +16,6 @@ Route::middleware('auth')->group(
                 Log::error('Error membuat role: ' . $e->getMessage());
             }
             return response()->json(['message' => 'Role berhasil buat!']);
-        })->name('make.role');
+        });
     }
 );
